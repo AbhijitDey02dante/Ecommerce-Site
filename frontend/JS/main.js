7be85fc7cde4e8f6c24efdf080543f90b5216e0b
AbhijitDey02dante <abhijitdey512@gmail.com>
2022-05-02 11:36:52 +0530

    Notification message

--- frontend/JS/main.js
+++ frontend/JS/main.js
@@ -1,6 +1,9 @@
 const navBtn=document.querySelector('#navButton');
 const navMenu=document.querySelector('#navMenu');
+const notification=document.querySelector('#notification');
+const test=document.querySelector('#test');
 
+//hamburger button
 navBtn.addEventListener('click',()=>{
     navMenu.classList.toggle('active');
     navBtn.classList.toggle('active');
@@ -13,3 +16,19 @@ navBtn.addEventListener('click',()=>{
         navBtn.innerText='☰';
     }
 })
+
+//notification message
+test.addEventListener('click',()=>{
+    notificationMessage("This is the message");
+})
+
+function notificationMessage(message){
+    const notif=document.createElement('div');
+    notif.innerText=message;
+
+    notification.appendChild(notif);
+
+    setTimeout(()=>{
+        notif.remove();
+    },3000);
+}
